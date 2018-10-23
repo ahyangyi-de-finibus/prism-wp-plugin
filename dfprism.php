62b89f494b3e86c9515cbc34a7a78bd654b810bf
Yi Yang @ Astraeus <ahyangyi@gmail.com>
2018-10-23 23:50:39 +0800

    Initial commit

---
+++ dfprism.php
@@ -0,0 +1,12 @@
+<?php
+/*
+* Plugin Name: Prism for De finibus
+* Description: Add a customized subset of Prism to my page
+* Version: 1.0
+* Author: Yi Yang
+* Author URI: https://blog.ahyangyi.org
+*/
+
+wp_enqueue_style("prism", plugin_dir_url( __FILE__ ) . "/" . "prism.css");
+wp_enqueue_script("prism", plugin_dir_url( __FILE__ ) . "/" . "prism.js");
+?>
