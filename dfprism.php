fce094dc289a531bd6a3749932466fc68ed135fe
Yi Yang @ Anteros <ahyangyi@gmail.com>
2025-05-23 13:52:50 +0800

    Bump the theme css

--- dfprism.php
+++ dfprism.php
@@ -2,7 +2,7 @@
 /*
 * Plugin Name: Prism for De finibus
 * Description: Add a customized subset of Prism to my page
-* Version: 1.3
+* Version: 1.4
 * Author: Yi Yang
 * Author URI: https://blog.ahyangyi.org
 */
