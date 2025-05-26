77c40b414cef05cba6ab43d6727fa480ad069d75
Yi Yang @ Anteros <ahyangyi@gmail.com>
2025-05-27 01:53:08 +0800

    v1.6: lighter text color; ligatures; code area borders

--- dfprism.php
+++ dfprism.php
@@ -2,7 +2,7 @@
 /*
 * Plugin Name: Prism for De finibus
 * Description: Add a customized subset of Prism to my page
-* Version: 1.5
+* Version: 1.6
 * Author: Yi Yang
 * Author URI: https://blog.ahyangyi.org
 */
